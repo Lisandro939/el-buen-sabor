63e23a5d9ec5d13000ff13e8c42dc762afb9fdb1
Lisandro939 <lisandrofernandez2705@gmail.com>
2023-09-07 10:48:11 -0300

    Small fixes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const abrirModalBtn = document.getElementById('abrirModal');
 const cerrarModalBtn = document.getElementById('RegistrarseButton');
 const modal = document.getElementById('miModal');
@@ -18,25 +17,4 @@ window.addEventListener('click', (e) => {
     if (e.target === modal) {
         modal.style.display = 'none';
 }
-=======
-const abrirModalBtn = document.getElementById('abrirModal');
-const cerrarModalBtn = document.getElementById('RegistrarseButton');
-const modal = document.getElementById('miModal');
-
-// Abrir el modal
-abrirModalBtn.addEventListener('click', () => {
-    modal.style.display = 'flex';
-});
-
-// Cerrar el modal
-cerrarModalBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
-});
-
-// Cerrar el modal haciendo clic fuera de él
-window.addEventListener('click', (e) => {
-    if (e.target === modal) {
-        modal.style.display = 'none';
-}
->>>>>>> 4066a1348176ee364b42543241188e5f29e4c0d4
 });
