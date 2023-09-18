4cef3d8883ef0ce96e39347fc739d6a33f473de0
Lisandro939 <lisandrofernandez2705@gmail.com>
2023-09-18 10:46:08 -0300

    Modal changes (Nico) - Ready

--- cashier/listOfOrders/listOfOrders.js
+++ cashier/listOfOrders/listOfOrders.js
@@ -1,8 +1,10 @@
 const abrirModalBtn = document.getElementById('button');
 const abrirModalMisDatosPersonales = document.getElementById('abrirModalMisDatosPersonales');
+const guardarButton = document.getElementById('guardarButton');
 
 const modal = document.getElementById('miModal');
 const modalMisDatosPersonales = document.getElementById('misDatosPersonales');
+const modalContraseña = document.getElementById('contraseñaModal');
 // Abrir el modal
 abrirModalBtn.addEventListener('click', () => {
     if (modal.style.display === 'flex') {
@@ -18,6 +20,10 @@ abrirModalMisDatosPersonales.addEventListener('click', () => {
     } else {
         modalMisDatosPersonales.style.display = 'flex';
     }
+});
+
+guardarButton.addEventListener('click', () => {
+    modalContraseña.style.display = 'none';
 });
 
 // Cerrar el modal haciendo clic fuera de él
